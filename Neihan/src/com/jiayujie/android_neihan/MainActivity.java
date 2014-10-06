89ba62f376da0a48997d75903dc809d7af0c940b
jiayujie <jiayujie@163.com>
2014-10-06 13:14:50 +0800

    2014年10月6日 13:14:46

--- Neihan/src/com/jiayujie/android_neihan/MainActivity.java
+++ Neihan/src/com/jiayujie/android_neihan/MainActivity.java
@@ -1,23 +1,53 @@
 package com.jiayujie.android_neihan;
 
+import java.security.acl.Group;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.jiayujie.android_neihan.fragments.HuodongFragment;
+import com.jiayujie.android_neihan.fragments.ImageListFragment;
+import com.jiayujie.android_neihan.fragments.MineFragment;
+import com.jiayujie.android_neihan.fragments.ReviewFragment;
+import com.jiayujie.android_neihan.fragments.TextListFragment;
+
+import android.R.integer;
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements OnCheckedChangeListener{
+
+	private RadioGroup group;
+	private List<Fragment> fragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		group = (RadioGroup)this.findViewById(R.id.main_tab_bar);
+		group.setOnCheckedChangeListener(this);
+		
+		fragments =new LinkedList<Fragment>();
+		fragments.add(new TextListFragment());
+		fragments.add(new ImageListFragment());
+		fragments.add(new ReviewFragment());
+		fragments.add(new HuodongFragment());
+		fragments.add(new MineFragment());
 	}
 
 	@Override
@@ -38,5 +68,41 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// TODO 自动生成的方法存根
+		int childCount=group.getChildCount();
+		int checkedIndex=0;
+		RadioButton btn=null;
+		for (int i = 0; i <childCount; i++) {
+			btn=(RadioButton)group.getChildAt(i);
+			if (btn.isChecked()) {
+				checkedIndex=i;
+				break;
+			}
+		}
+		Fragment fragment=fragments.get(checkedIndex);
+		
+		FragmentManager manager=getSupportFragmentManager();
+		FragmentTransaction transaction=manager.beginTransaction();
+		transaction.replace(R.id.main_fragment_container, fragment);
+		transaction.commit();
+		
+		/*switch (checkedIndex) {
+		case 0:
+			
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+		transaction.commit();*/
 	}
 }
---
+++ Neihan/src/com/jiayujie/android_neihan/fragments/HuodongFragment.java
@@ -0,0 +1,46 @@
+package com.jiayujie.android_neihan.fragments;
+
+import com.jiayujie.android_neihan.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HuodongFragment extends Fragment {
+
+	public HuodongFragment() {
+		// TODO 自动生成的构造函数存根
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		super.onCreate(savedInstanceState);
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO 自动生成的方法存根
+		View view = inflater.inflate(R.layout.fragment_huodonglist, container,false);
+		return view;
+	}
+	
+	@Override
+	public void onResume() {
+		// TODO 自动生成的方法存根
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		// TODO 自动生成的方法存根
+		super.onPause();
+	}
+	
+	@Override
+	public void onDestroyView() {
+		// TODO 自动生成的方法存根
+		super.onDestroyView();
+	}
+}
